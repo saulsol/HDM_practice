ff6567df3182d8663da65989f1210f748789d740
saul <dlathf3210@naver.com>
2023-08-06 19:23:08 +0900

    START

---
+++ src/main/java/com/example/hdm_practice/HdmPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdm_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdmPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdmPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hdm_practice/HdmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdm_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdmPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
