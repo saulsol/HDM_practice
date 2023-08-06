863a116dbbe1a9dc7d7b7e9637db3046b5a6922d
saul <dlathf3210@naver.com>
2023-08-07 08:57:08 +0900

    mockMvc contentType test

--- src/main/java/com/example/hdm_practice/controller/PostController.java
+++ src/main/java/com/example/hdm_practice/controller/PostController.java
@@ -2,15 +2,18 @@ package com.example.hdm_practice.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
+    @PostMapping("/posts")
     public String getMappingPractice(){
         return "HI";
     }
+
+
 
 
 
--- src/test/java/com/example/hdm_practice/controller/PostControllerTest.java
+++ src/test/java/com/example/hdm_practice/controller/PostControllerTest.java
@@ -3,15 +3,15 @@ package com.example.hdm_practice.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest
 class PostControllerTest {
@@ -23,10 +23,17 @@ class PostControllerTest {
     @Test
     @DisplayName("/post 요청시 hi 출력")
     void test() throws Exception  {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED ) //application/x-www-form-urlencoded.
+
+                ) // application/json
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("HI"));
+                .andExpect(MockMvcResultMatchers.content().string("HI"))
+                .andDo(print());
     }
+    // 대소문자가 달라도 틀렸다고 에러를 띄움
+    // MockMVC는 기본적으로 application/json
+    // contentType 이 바뀌어도 기본적으로 Test 통과가 된다.
 
 
 }
