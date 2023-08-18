6037edb33c991677716799b88dee8b8b91ca5cb3
saul <dlathf3210@naver.com>
2023-08-18 09:04:11 +0900

    0818

--- src/main/java/com/example/hdm_practice/controller/PostController.java
+++ src/main/java/com/example/hdm_practice/controller/PostController.java
@@ -1,10 +1,7 @@
 package com.example.hdm_practice.controller;
 
 
-import com.example.hdm_practice.request.PostCreateDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
